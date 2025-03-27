528fbb15fc0eda4db008840cb2f26e028529e028
Maria <maryallbond@gmail.com>
2025-03-27 14:50:12 +0200

    update by functional

--- src/component/GlobalStyled.jsx
+++ src/component/GlobalStyled.jsx
@@ -2,6 +2,4 @@ import { createGlobalStyle } from "styled-components";
 import "reset-css";
 import "modern-normalize";
 
-export const GlobalStyles = createGlobalStyle`
-
-`;
+export const GlobalStyles = createGlobalStyle``;
